04b0f4e3fc1a33845038c214cb44a90b52fde88f
renv39 <renv.seneca@gmail.com>
2026-05-21 17:24:28 -0400

    refactor(home): simplify layout by merging Box and Stack, remove redundant styles

--- foundit-ui/app/page.tsx
+++ foundit-ui/app/page.tsx
@@ -15,23 +15,33 @@ export default function Home() {
       justifyContent="center"
       bg="bg.subtle"
     >
-      <Box bg="bg" rounded="xl" shadow="md" p={8} w="full" maxW="sm">
-        <Stack gap={6} alignItems="center" textAlign="center">
-          <Stack gap={1}>
-            <Heading size="4xl">Welcome to FoundIt</Heading>
-            <Text color="fg.muted" fontSize="sm">
-              Please login to your account to continue
-            </Text>
-          </Stack>
-          <Button px={16} colorPalette="red" onClick={handleSignIn}>
-            Login
-          </Button>
-          <Stack gap={0}>
-            <Text>Lost and found office hours:</Text>
-            <Text>Monday to Friday: 9:00 AM - 5:00 PM</Text>
-          </Stack>
+      <Stack
+        bg="bg"
+        rounded="xl"
+        shadow="md"
+        p={12}
+        w="full"
+        maxW="md"
+        minH="55vh"
+        justify="space-between"
+        alignItems="center"
+        textAlign="center"
+      >
+        <Heading size="4xl">Welcome to FoundIt</Heading>
+
+        <Text color="fg.muted" fontSize="sm">
+          Please login to your account to continue
+        </Text>
+
+        <Button px={16} colorPalette="red" onClick={handleSignIn}>
+          Login
+        </Button>
+
+        <Stack gap={1} fontSize="sm">
+          <Text fontWeight="medium">Lost and found office hours:</Text>
+          <Text color="fg.muted">Mon. to Fri. 9:00 AM - 5:00 PM</Text>
         </Stack>
-      </Box>
+      </Stack>
     </Box>
   );
 }
